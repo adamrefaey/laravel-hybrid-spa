b97dff8d9121367222fd26543cce2eaecc7dec0d
mustafarefaey <mustafarefaey@users.noreply.github.com>
2020-11-30 01:21:13 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace MustafaRefaey\LaravelHybrid\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use MustafaRefaey\LaravelHybrid\LaravelHybridServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
